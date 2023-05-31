8eeea027557cf860c52fb96662d22d9e909a09a6
Christoph Wille <christoph.wille@gmail.com>
2023-05-31 10:01:00 +0200

    Generate Storage account SAS via accountName/accountKey StorageSharedKeyCredential

--- StorageAccountAccessKeyGen/Program.cs
+++ StorageAccountAccessKeyGen/Program.cs
@@ -5,6 +5,7 @@ using Azure.Storage.Blobs;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace StorageAccountAccessKeyGen
 {
@@ -22,12 +23,18 @@ namespace StorageAccountAccessKeyGen
             IConfigurationRoot configuration = builder.Build();
             string storageConnectionString = configuration["storageConnectionString"];
 
-
+            // Simple file SAS URI
             var blobClient = GetBlobContainerClient(storageConnectionString, DemoContainer);
             bool canGenerateSasUri = blobClient.CanGenerateSasUri;
             Uri sasUri = GenerateSasReadUriForFile(DemoFile, 5, blobClient);
 
-            // TODO
+            // Storage account SAS URI
+            var kvPairs = GetKeyValuePairsFromStorageConnectionString(storageConnectionString);
+            var accountName = kvPairs["AccountName"];
+            var accountKey = kvPairs["AccountKey"];
+            var blobServiceEndpoint = $"https://{accountName}.blob.core.windows.net";
+            Uri accountSasUri = GetAccountSas(accountName, accountKey, blobServiceEndpoint, 10);
+
         }
 
         static BlobContainerClient GetBlobContainerClient(string storageConnectionString, string containerName)
@@ -50,6 +57,43 @@ namespace StorageAccountAccessKeyGen
 
             var blobClient = blobContainerClient.GetBlobClient(fileName);
             return blobClient.GenerateSasUri(sasBuilder);
+        }
+
+        // https://www.craftedforeveryone.com/beginners-guide-and-reference-to-azure-blob-storage-sdk-v12-dot-net-csharp/#get_account_name_and_account_key_from_a_connection_string
+        static Dictionary<string, string> GetKeyValuePairsFromStorageConnectionString(string storageConnectionString)
+        {
+            var settings = new Dictionary<string, string>();
+
+            foreach (var nameValue in storageConnectionString.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                var splittedNameValue = nameValue.Split(new char[] { '=' }, 2);
+                settings.Add(splittedNameValue[0], splittedNameValue[1]);
+            }
+
+            return settings;
+        }
+
+        // https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/storage/Azure.Storage.Blobs/samples/Sample02_Auth.cs
+        static Uri GetAccountSas(string storageAccountName, string storageAccountKey, string blobServiceEndpoint, int expiresInHours)
+        {
+            AccountSasBuilder sas = new AccountSasBuilder
+            {
+                Services = AccountSasServices.Blobs,
+
+                // F12 on AccountSasResourceTypes - to be able to download files from containers in a Storage account we need all of those
+                ResourceTypes = AccountSasResourceTypes.All,
+                ExpiresOn = DateTimeOffset.UtcNow.AddHours(expiresInHours),
+                Protocol = SasProtocol.Https
+            };
+
+            // Allow read access (list is needed because otherwise Storage Explorer won't connect at all)
+            sas.SetPermissions(AccountSasPermissions.Read | AccountSasPermissions.List);
+
+            StorageSharedKeyCredential credential = new StorageSharedKeyCredential(storageAccountName, storageAccountKey);
+            UriBuilder sasUri = new UriBuilder($"{blobServiceEndpoint}");
+            sasUri.Query = sas.ToSasQueryParameters(credential).ToString();
+
+            return sasUri.Uri;
         }
     }
 }
